3cb79f6859dfaa739100782e4cfaa9bfb7c6d178
Shwetank <84504016+Shwetank-Chaudhary@users.noreply.github.com>
2026-04-19 12:20:08 +0530

    Time: 340 ms (73.32%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -2,8 +2,15 @@
 select 
 q.query_name,
 round(avg(q.rating/q.position),2) as quality, 
--- (select count(rating) from queries WHERE q.query_name  = query_name) as total,
--- (select count(rating) from queries where rating<3 and q.query_name  = query_name) as le3,
-round((select count(rating) from queries where rating < 3 and q.query_name  = query_name)/(select count(rating) from queries where q.query_name  = query_name)*100,2) as poor_query_percentage
+-- round((select count(rating) from queries where rating < 3 and q.query_name  = query_name)/(select count(rating) from queries where q.query_name  = query_name)*100,2) as poor_query_percentage
+
+round(
+    sum(
+        case 
+        when q.rating<3 then 1 else 0 end
+    )*100/count(*),
+    2
+)
+as poor_query_percentage
 from queries as q
 group by query_name
