35a3ae5c979afebe04373aff9a56aca7cbea36db
Shwetank <84504016+Shwetank-Chaudhary@users.noreply.github.com>
2026-04-23 08:22:59 +0530

    Time: 459 ms (18.79%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -4,5 +4,4 @@ select
         when (select num from mynumbers group by num having count(num) = 1 order by num desc limit 1) is not null       then         (select num from mynumbers group by num having count(num) = 1 order by num desc limit 1)
         else 
             null 
-    end as num
-    from mynumbers limit 1
+    end as num;
