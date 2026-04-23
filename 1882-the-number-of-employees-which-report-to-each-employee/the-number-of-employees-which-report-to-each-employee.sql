35d1faa5a52812e8ec8e004316ba5bc9ae4027ff
Shwetank <84504016+Shwetank-Chaudhary@users.noreply.github.com>
2026-04-23 08:53:15 +0530

    Time: 1811 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -5,6 +5,4 @@ select
     (select count(*) from employees as e where e.reports_to = e1.employee_id) as reports_count,
     (select round(avg(e.age)) from employees as e where e.reports_to = e1.employee_id) as average_age
     from employees as e1 
-    cross join employees as e2
-    on e1.employee_id = e2. reports_to
-group by e1.employee_id order by e1.employee_id
+group by e1.employee_id having reports_count >=1 order by e1.employee_id
