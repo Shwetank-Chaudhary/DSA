cd2dfa9a3d8875838ef0205f05427ff6ffe9ea42
Shwetank <84504016+Shwetank-Chaudhary@users.noreply.github.com>
2026-04-25 15:38:39 +0530

    Time: 1491 ms (66.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,9 @@
+select 
+    'Low Salary' as category ,
+    count(if(income<20000,1,null)) as accounts_count from Accounts
+    union all 
+    select 'Average Salary',
+    count(if(income between 20000 and 50000,1,null)) from Accounts
+    union all
+    select 'High Salary',
+    count(if(income>50000,1,null)) from Accounts
