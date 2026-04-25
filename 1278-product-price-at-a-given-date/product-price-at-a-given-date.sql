3f62eae89e7ae9c2bbb4181df278d7aa91805222
Shwetank <84504016+Shwetank-Chaudhary@users.noreply.github.com>
2026-04-25 14:42:12 +0530

    Time: 1576 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select 
+    p1.product_id,
+    ifnull(
+        (
+            select new_price from products where products.product_id  = p1.product_id 
+            and change_date <= Date('2019-08-16') order by change_date desc limit 1
+        ),10)
+    as price
+from products p1
+-- right join products as p2
+-- on p1.product_id =  p2.product_id
+-- and p1.change_date <= Date('2019-08-16')
+group by p1.product_id 
